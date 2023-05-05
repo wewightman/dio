ce04553ccd3c6f0235dca2e6c6baf2d2b13942d5
Wren Wightman <wren.wightman@duke.edu>
2023-05-05 16:44:59 -0400

    Package3 structuring

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 with open("README.md", 'r') as f:
     readme=f.read()
-    
+
 setup(
     name = 'dio',
     description = "Data Input and Output functions for binary datafiles and parameter files",
@@ -10,5 +10,6 @@ setup(
     author_name = "Wren Wightman",
     author_email = "wew12@duke.edu",
     license = 'MIT',
-    packages = ['pycbf']
+    packages = ['dio'],
+    requires=['mat73']
 )
