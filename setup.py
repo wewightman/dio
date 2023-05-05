b83e8438a9833898b26029c8f40df7cae1ee7540
Wren Wightman <wren.wightman@duke.edu>
2023-05-05 16:21:08 -0400

    Created setup.py file for packaging

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
+
+with open("README.md", 'r') as f:
+    readme=f.read()
+    
 setup(
-    name='pycbf',
-    description="C-Backed beamforming engines",
-    author_name="Wren Wightman",
-    author_email="wew12@duke.edu",
-    packages=['pycbf']
+    name = 'dio',
+    description = "Data Input and Output functions for binary datafiles and parameter files",
+    long_description = readme,
+    author_name = "Wren Wightman",
+    author_email = "wew12@duke.edu",
+    license = 'MIT',
+    packages = ['pycbf']
 )
